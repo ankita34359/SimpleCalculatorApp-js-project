214832b42c27b0e5240aea4f3feddcc2bc7a4561
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2023-04-23 12:34:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Ask user for the first number
+
+let num1 = parseInt(prompt("Enter the First Number"));
+let num2 = parseInt(prompt("Enter the Second Number"));
+
+// Ask for Operator
+let operator = prompt("Enter the operator (+, -,*,/):");
+
+
+
+
+let result;
+if(operator === "+"){
+  result = num1 + num2;
+}else if(operator === "-"){
+  result = num1 - num2;
+}else if(operator === "*"){
+  result = num1 * num2;
+}else if(operator === "/"){
+  result = num1 / num2;
+}else{
+  result = "Invalid Operator";
+}
+
+// Display the result in console
+console.log(`Result: ${result}`); 
+// backtick
